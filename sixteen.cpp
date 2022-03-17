051c8065be4d7764243cce51c6c9bf3cae4eee98
heerpatell <68782787+heerpatell@users.noreply.github.com>
2022-03-17 15:00:00 +0530

    commit

--- sixteen.cpp
+++ sixteen.cpp
@@ -1,24 +1,21 @@
-//       1 
-//     2 3
-//   4 5 6
-// 7 8 9 10
 #include <iostream>
 using namespace std;
-int main()
-{
-    int n,count=1;
-    cin >> n; // no of rows
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {            
-            if(i>=n-j+1){
-                cout<<count<<" ";
-                count++;
-            }else{
-                cout<<"  ";
-            }
-        }
-        cout<<endl;
-    }
+
+int main(){
+	int r,num=1;
+	cout<<"\nEnter Rows : ";
+    cin>>r;
+    
+    for(int i=1;i<=r;i++){
+    	for(int j=1;j<=r;j++){
+    		if(j<(r-i)+1){
+    			cout<<"  "; 
+			}else{
+				cout<<num<<" ";
+				num++;
+			}
+		}
+		cout<<"\n";    
+	}
+	return 0;
 }
