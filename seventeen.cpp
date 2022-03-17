39dd425ad011e6925bdcd15a69ea60913fe15c49
nikita-mirchandani <82458994+nikita-mirchandani@users.noreply.github.com>
2022-03-17 16:09:52 +0530

    commit

--- seventeen.cpp
+++ seventeen.cpp
@@ -1,4 +1,4 @@
-// hollow triangle
+// hollow pyramid/triangle
 #include <iostream>
 using namespace std;
 int main()
@@ -16,7 +16,7 @@ int main()
             }
         }
 
-        for (int j = n+1; j <= (2*n)-1; j++) //nto 2n-1
+        for (int j = n+1; j <= (2*n)-1; j++) //n+1 to 2n-1
         {     
             if(i==j-n+1 || (i==n && j%2!=0)){
                 cout<<"*";
